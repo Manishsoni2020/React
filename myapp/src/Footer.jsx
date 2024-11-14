8fdef74c4d8abde8edda30a37f5b7fa4cb07bf48
Manishsoni2020 <sonimanish24163@gmail.com>
2024-11-14 15:33:43 +0530

    REACT

---
+++ myapp/src/Footer.jsx
@@ -0,0 +1,25 @@
+// rafce
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      this is footer
+    </div>
+  )
+}
+
+export default Footer
+
+// // rfce
+// import React from 'react'
+
+// function Footer() {
+//   return (
+//     <div>
+      
+//     </div>
+//   )
+// }
+
+// export default Footer
