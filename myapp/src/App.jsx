86ec762858d2a24fba1682446fb92c4f113e2f85
Manishsoni2020 <sonimanish24163@gmail.com>
2024-11-15 17:14:17 +0530

    React

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,19 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './Header.jsx'
-import Footer from './Footer.jsx'
+import  './App.css'
+// import Header from './Header.jsx'
+// import Footer from './Footer.jsx'
+import Headername from './Headername.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Manish Soni</h1>
-      <Header/>
-      <Footer/>
-      <Footer/>
+      <h1>this is h1 tag</h1>
     </>
   )
   
---
+++ myapp/src/Headername.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Headernamecss from './headername.css'
+
+const Headername = () => {
+  return (
+    <>
+    <div class="headername">
+      <hi>Hi my name is manish soni</hi>
+      <h2>java developer</h2>
+    </div>
+    </>
+  )
+}
+
+export default Headername
